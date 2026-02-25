29655f265910c9e5ba4bd234a4cb83c23fa7252b
Thomas Fuller <tfull013@odu.edu>
2026-02-24 19:15:34 -0500

    added po inserts to database

--- models/invoice.py
+++ models/invoice.py
@@ -5,7 +5,7 @@ class Invoice(db.Model):
     __tablename__ = "invoice"
 
     id = db.Column(db.Integer, primary_key=True)
-    po_number = db.Column(db.Integer, db.ForeignKey("purchase_orders.id"), nullable=False)
+    po_number = db.Column(db.Integer, db.ForeignKey("purchase_orders.id"), nullable=True)
     vendor = db.Column(db.Integer, db.ForeignKey("vendors.id"), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     status = db.Column(db.String(50), default="pending")
