247da74b75500fabd3b21716bb95efb35a3abdb4
MichaelNimitz1995 <mnimi001@odu.edu>
2026-03-28 18:20:46 -0400

    most confidence score logic done, results added to matches table

--- po_matching/matcher.py
+++ po_matching/matcher.py
@@ -2,25 +2,20 @@ from models.purchase_orders import Purchase_Order
 
 
 def match_invoice(invoice):
-    if invoice.po_number:
-        po = match_to_po_directly(invoice.po_number)
-        if po:
-            return po, 100
+    po_and_score = {}
 
-    po = match_by_fields(invoice)
-    if po:
-        return po, 100
+    results = Purchase_Order.query.all()
+    for row in results:
+        score = 0
+        if invoice.po_number == row.po_number:
+            score = score+50
+        if invoice.vendor == row.vendor:
+            score = score+20
+        if invoice.amount == row.amount:
+            score = score+15
+        if invoice.date_issued == row.date_issued:
+            score = score+15
 
-    return None, None
-
-
-def match_to_po_directly(po_number):
-    return Purchase_Order.query.filter_by(po_number=po_number).first()
-
-
-def match_by_fields(invoice):
-    return Purchase_Order.query.filter_by(
-        vendor=invoice.vendor,
-        amount=invoice.amount,
-        date_issued=invoice.date_issued,
-    ).first()
+        po_and_score.update({row:score})
+    
+    return po_and_score
--- po_matching/run_matching.py
+++ po_matching/run_matching.py
@@ -1,5 +1,6 @@
 from extensions import db
 from models.invoice import Invoice
+from models.matches import Match
 from po_matching.matcher import match_invoice
 
 
@@ -9,11 +10,20 @@ def run_matching():
 
     matched = 0
     for invoice in unmatched:
-        po, score = match_invoice(invoice)
-        if po:
-            invoice.matched_po_id = po.id
-            invoice.confidence_score = score
-            matched += 1
+        matchFound = False
+        po_and_score = match_invoice(invoice)
+        for currentMatch in po_and_score:
+            if currentMatch.score > 25:
+                match = Match(
+                    invoice_id = invoice.id,
+                    po_id = currentMatch.row.id,
+                    confidence_score = currentMatch.score,
+                    matchFound = True
+                )
+                db.session.add(match)
+
+        if matchFound:
+            matched +=1
 
     db.session.commit()
     print(f"Matched: {matched}, still unmatched: {len(unmatched) - matched}")
