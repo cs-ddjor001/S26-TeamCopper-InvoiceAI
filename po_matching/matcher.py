a78310e3eb28f6bef43c2e7cdbaa7cfef2ac7384
MichaelNimitz1995 <mnimi001@odu.edu>
2026-03-29 18:47:25 -0400

    matches table now gets populated

--- po_matching/matcher.py
+++ po_matching/matcher.py
@@ -5,17 +5,22 @@ def match_invoice(invoice):
     po_and_score = {}
 
     results = Purchase_Order.query.all()
-    for row in results:
+    for po in results:
         score = 0
-        if invoice.po_number == row.po_number:
+        if invoice.po_number == po.po_number:
+            # print("PO number match")
             score = score+50
-        if invoice.vendor == row.vendor:
+        if invoice.vendor == po.vendor:
+            # print("Vendor match")
             score = score+20
-        if invoice.amount == row.amount:
+        if invoice.amount == po.amount:
+            # print("AMount match")
             score = score+15
-        if invoice.date_issued == row.date_issued:
+        if invoice.date_issued == po.date_issued:
+            # print("Date match")
             score = score+15
-
-        po_and_score.update({row:score})
+        # if score > 0:
+            # print(f"Score is {score}")
+        po_and_score.update({po:score})
     
     return po_and_score
--- po_matching/run_matching.py
+++ po_matching/run_matching.py
@@ -7,19 +7,19 @@ from po_matching.matcher import match_invoice
 def run_matching():
     unmatched = Invoice.query.filter_by(matched_po_id=None).all()
     print(f"Found {len(unmatched)} unmatched invoice(s).")
-
     matched = 0
     for invoice in unmatched:
         matchFound = False
         po_and_score = match_invoice(invoice)
-        for currentMatch in po_and_score:
-            if currentMatch.score > 25:
+        for po, score in po_and_score.items():
+            # TODO need to prevent duplicate matches?
+            if score > 25:
                 match = Match(
                     invoice_id = invoice.id,
-                    po_id = currentMatch.row.id,
-                    confidence_score = currentMatch.score,
-                    matchFound = True
+                    po_id = po.id,
+                    confidence_score = score,
                 )
+                matchFound = True
                 db.session.add(match)
 
         if matchFound:
